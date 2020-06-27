e988ff7f46c44bba3904c3d8cf9087398c1f5f36
Leigh Larson <leighlarson4@gmail.com>
2020-06-27 00:29:58 -0600

    Make Lint edits

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 const data = require('../src/data');
@@ -14,7 +12,6 @@ describe('Game', function() {
     game = new Game();
     cards = game.createCards(prototypeQuestions); 
     deck = new Deck(cards);
-    // turn = new Turn(guess, cards[0]);
     round = new Round(deck);
     game.start();
   });
@@ -23,7 +20,7 @@ describe('Game', function() {
     expect(game.currentRound).to.deep.equal(round);
   });
 
-  it('should create and put cards in deck, and create new round', function() {
+  it('should create and put cards in deck and create new round', function() {
     expect(game.currentRound.deck).to.deep.equal(round.deck);
   });
 
