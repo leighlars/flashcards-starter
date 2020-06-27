fcc2319ded287b7f0373103caf432eff213fbf32
Leigh Larson <leighlarson4@gmail.com>
2020-06-26 21:10:28 -0600

    Change assert to expect syntax in turn test

--- src/Turn.js
+++ src/Turn.js
@@ -4,28 +4,28 @@ class Turn {
     this.card = card;
   }
 
+  // returnGuess: method that returns the guess
   returnGuess() {
     return this.guess;
   }
 
+  // returnCard: method that returns the Card
   returnCard() {
     return this.card;
   }
 
+  // evaluateGuess: method that returns a boolean indicating if the user’s 
+  // guess matches the correct answer on the card
   evaluateGuess() {
     return this.guess === this.card.correctAnswer ? true : false;
   }
 
+  // giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ 
+  // based on whether the guess is correct or not.
   giveFeedback() {
     return this.guess === this.card.correctAnswer ? 'correct!' : 'incorrect!';
   }
 
 }
 
-// returnGuess: method that returns the guess
-// returnCard: method that returns the Card
-// evaluateGuess: method that returns a boolean indicating if the user’s 
-// guess matches the correct answer on the card
-// giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ 
-// based on whether the guess is correct or not.
 module.exports = Turn;
--- test/turn-test.js
+++ test/turn-test.js
@@ -1,9 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
-
 const Turn = require('../src/Turn');
-const { assert } = require('chai');
 
 describe('Turn', function() {
 
@@ -17,47 +15,48 @@ describe('Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
+    // returnGuess: method that returns the guess
   it('should return a guess', function() {
     const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
     const turn = new Turn('Li', card);
-      
-    let returnedGuess = turn.returnGuess();
-
-    assert.equal(returnedGuess, 'Li');
+  
+    expect(turn.returnGuess()).to.equal( 'Li');
   }); 
 
+  // returnCard: method that returns the Card
   it('should return the card', function () {
     const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
     const turn = new Turn('Li', card);
 
-    let returnedCard = turn.returnCard();
-    assert.equal(returnedCard, card);
+    expect(turn.returnCard()).to.equal(card);
   }); 
 
-  it('should return whether user guessed correctly', function () {
-    const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn = new Turn('Li', card);
-
-    let evaluation = turn.evaluateGuess();
-
-    assert.equal(evaluation, false);
-  }); 
-
-  it('should give feedback if guess is correct or not', function () {
-    const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn = new Turn('Li', card);
-
-    let feedback =  turn.giveFeedback();
-
-    assert.equal(feedback, 'incorrect!');
-  }); 
-
-  // returnGuess: method that returns the guess
-  // returnCard: method that returns the Card
   // evaluateGuess: method that returns a boolean indicating if the user’s 
   // guess matches the correct answer on the card
+  it('should return whether user guessed correctly', function () {
+    const card1 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn1 = new Turn('Li', card1);
+
+    expect(turn1.evaluateGuess()).to.equal(false);
+
+    const card2 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn2 = new Turn('Leigh', card2);
+
+    expect(turn2.evaluateGuess()).to.equal(true);
+  }); 
+
   // giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ 
   // based on whether the guess is correct or not.
+  it('should give feedback if guess is correct or not', function () {
+    const card1 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn1 = new Turn('Li', card1);
 
+    expect(turn1.giveFeedback()).to.equal( 'incorrect!');
+
+    const card2 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn2 = new Turn('Leigh', card2);
+
+    expect(turn2.giveFeedback()).to.equal('correct!');
+  }); 
 
 });
