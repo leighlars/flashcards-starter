61103a839b50f69b4a36541016a31d0290c4d461
Leigh Larson <leighlarson4@gmail.com>
2020-06-26 19:30:27 -0600

    Pseudo / attempt code for round file and test

---
+++ test/round-test.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { assert } = require('chai');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', function () {
+
+  it('should be a function', function () {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function () {
+    const round = new Round();
+
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should be initialized with a deck', function () {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    assert.deepEqual(round.deck, [card1, card2, card3]);
+
+  });
+
+  it.skip('should return number of cards in deck', function () {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+
+    let numCards = deck.countCards();
+
+    assert.equal(numCards, 3);
+  });
+
+
+});
