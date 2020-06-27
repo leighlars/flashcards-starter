23e1a8175283179ad7ca9dac8dc9c8ee66dc7903
Leigh Larson <leighlarson4@gmail.com>
2020-06-26 21:41:26 -0600

    Refactor tests for accuracy and clarity

--- test/turn-test.js
+++ test/turn-test.js
@@ -15,47 +15,45 @@ describe('Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-    // returnGuess: method that returns the guess
+  // returnGuess: method that returns the guess
   it('should return a guess', function() {
     const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn = new Turn('Li', card);
+    const turn1 = new Turn('Li', card);
+    const turn2 = new Turn('Leigh', card);
   
-    expect(turn.returnGuess()).to.equal( 'Li');
+    expect(turn1.returnGuess()).to.equal( 'Li');
+    expect(turn2.returnGuess()).to.equal('Leigh');
   }); 
 
   // returnCard: method that returns the Card
   it('should return the card', function () {
     const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn = new Turn('Li', card);
+    const turn1 = new Turn('Li', card);
+    const turn2 = new Turn('Leigh', card);
 
-    expect(turn.returnCard()).to.equal(card);
+    expect(turn1.returnCard()).to.equal(card);
+    expect(turn2.returnCard()).to.equal(card);
   }); 
 
   // evaluateGuess: method that returns a boolean indicating if the user’s 
   // guess matches the correct answer on the card
   it('should return whether user guessed correctly', function () {
-    const card1 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn1 = new Turn('Li', card1);
+    const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn1 = new Turn('Li', card);
+    const turn2 = new Turn('Leigh', card);
 
     expect(turn1.evaluateGuess()).to.equal(false);
-
-    const card2 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn2 = new Turn('Leigh', card2);
-
     expect(turn2.evaluateGuess()).to.equal(true);
   }); 
 
   // giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ 
   // based on whether the guess is correct or not.
   it('should give feedback if guess is correct or not', function () {
-    const card1 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn1 = new Turn('Li', card1);
+    const card = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
+    const turn1 = new Turn('Li', card);
+    const turn2 = new Turn('Leigh', card);
 
     expect(turn1.giveFeedback()).to.equal( 'incorrect!');
-
-    const card2 = new Card(1, 'What is my name?', ['Leigh', 'Lee', 'Li'], 'Leigh');
-    const turn2 = new Turn('Leigh', card2);
-
     expect(turn2.giveFeedback()).to.equal('correct!');
   }); 
 
