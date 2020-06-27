d7fb8e7f42b930a0f32b2afa4d0e2dba147d68db
Leigh Larson <leighlarson4@gmail.com>
2020-06-26 23:34:59 -0600

    Delete  notes from turn file

--- src/Turn.js
+++ src/Turn.js
@@ -4,24 +4,18 @@ class Turn {
     this.card = card;
   }
 
-  // returnGuess: method that returns the guess
   returnGuess() {
     return this.guess;
   }
 
-  // returnCard: method that returns the Card
   returnCard() {
     return this.card;
   }
 
-  // evaluateGuess: method that returns a boolean indicating if the user’s 
-  // guess matches the correct answer on the card
   evaluateGuess() {
     return this.guess === this.card.correctAnswer;
   }
 
-  // giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ 
-  // based on whether the guess is correct or not.
   giveFeedback() {
     return this.evaluateGuess() ? 'correct!' : 'incorrect!';
   }
